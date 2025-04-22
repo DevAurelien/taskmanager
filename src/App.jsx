391f6626e355bd88d2d45fa799c6495ad23c6562
AdminArchon <lhuillier.aurelien47@gmail.com>
2025-04-22 23:17:19 +0200

    AppTaskManager

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Task from "./components/task.jsx";
+
+function App() {
+  return (
+    // crée une tache
+    <Task />
+    // affiche les taches
+    // supprimer taches
+  );
+}
+
+export default App;
---
+++ src/components/task.jsx
@@ -0,0 +1,7 @@
+function Task() {
+  return (
+<div className="bg-zinc-600 h-full w-full">
+      <h1>bonjour</h1>
+    </div>)
+}
+export default Task
