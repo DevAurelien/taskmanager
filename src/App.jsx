c116ca617f7492b5b9424192bba9c68ea600cc9a
AdminArchon <lhuillier.aurelien47@gmail.com>
2025-04-24 23:02:46 +0200

    Fonctionnel optimisé 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import Task from "./components/task.jsx";
-import CreateTask from "./components/createTask.jsx";
-import { useState } from "react";
 
 function App() {
 
-  const [tasks,setTasks]=useState([]);
-
   return (
-    <div className="flex flex-col text-4xl text-white bg-black w-full h-full items-center">
+    <div className="overflow-y-scroll flex flex-col text-4xl text-white bg-black w-full h-full items-center">
       <h1 className="py-10">Manager de Taches</h1>
-    {/* // crée une tache partie superieur */}
-    <CreateTask />
-    {/* // affiche les taches */}
     <Task/>
     {/* editer les taches */}
     {/* // supprimer taches */}
--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,19 +1,45 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function Task() {
-  const [tasks, setTasks] = useState(["tache 1", "tache 2"]);
+  
+  const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    document.querySelector('#taches').innerHTML = tasks.map((tache, index) => `<div class="flex bg-blue-600 rounded-2xl mt-5 h-10 w-full justify-center items-center"><h1 key=${index}>${tache}</h1></div>`).join("");
-  }, [tasks]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const usedTask = e.target[0].value.trim();
+    if (!tasks.includes(usedTask)) {
+      setTasks([...tasks, usedTask]);
+    }
+    e.target[0].value = "";
+  };
 
   return (
-    <div className="flex flex-col text-white font-bold text-2xl mt-20 w-1/2">
-      <div id="taches">
-        
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex relative text-white font-bold text-xl bg-blue-600 rounded-2xl mt-5 h-10 w-1/2 pl-4 py-1 justify-between items-center"
+      >
+        <input
+          focus
+          type="text"
+          className="resize-none h-full flex w-7/8 bg-transparent outline-none"
+        ></input>
+        <button className="absolute rotate-90 text-4xl center right-0 w-12 h-12">
+          +
+        </button>
+      </form>
+      <div className="flex flex-col text-white font-bold text-2xl mt-20 w-1/2">
+        {tasks.map((displayTask, index) => (
+          <div
+            key={index}
+            className="flex text-xl bg-blue-600 rounded-2xl mt-5 h-10 w-full pl-4 justify-start items-center"
+          >
+            <h1>{displayTask}</h1>
+          </div>
+        ))}
       </div>
-      {/* {tasks.map((e, index) => (<div className="flex bg-blue-600 rounded-2xl mt-5 h-10 w-full justify-center items-center"><h1 key={index}>{e}</h1></div>))} */}
-    </div>
+    </>
   );
 }
 export default Task;
+//
