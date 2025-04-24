848a551530f039e8ee7a3f9b91b689fc359c0d5c
AdminArchon <lhuillier.aurelien47@gmail.com>
2025-04-24 22:14:13 +0200

    Ebauche 2

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,12 +1,18 @@
+import { useEffect, useState } from "react";
+
 function Task() {
+  const [tasks, setTasks] = useState(["tache 1", "tache 2"]);
 
-    // function createTask = ()=>{
-
-    // }
+  useEffect(() => {
+    document.querySelector('#taches').innerHTML = tasks.map((tache, index) => `<div class="flex bg-blue-600 rounded-2xl mt-5 h-10 w-full justify-center items-center"><h1 key=${index}>${tache}</h1></div>`).join("");
+  }, [tasks]);
 
   return (
-    <div className="flex text-white font-bold text-2xl bg-blue-600 rounded-2xl mt-5 h-20 w-1/2 justify-center items-center">
-      <h1></h1>
+    <div className="flex flex-col text-white font-bold text-2xl mt-20 w-1/2">
+      <div id="taches">
+        
+      </div>
+      {/* {tasks.map((e, index) => (<div className="flex bg-blue-600 rounded-2xl mt-5 h-10 w-full justify-center items-center"><h1 key={index}>{e}</h1></div>))} */}
     </div>
   );
 }
